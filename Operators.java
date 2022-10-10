425599ce991a68e3d6eb5ff93b3b88d6b486e155
DHCJS <hartatociptajaya@gmail.com>
2022-10-10 11:30:56 +0700

    Operators in Java

---
+++ Operators.java
@@ -0,0 +1,21 @@
+class Operators {
+    public static void main(String[] )args {
+        int number1 = 18;
+        int number2 = 3;
+
+        // addition
+        system.out.println(number1 + number2); // 21
+
+        // substraction
+        system.out.println(number1 - number2); // 15
+
+        // multiplication
+        system.out.println(number1 * number2); // 54
+
+        // division
+        system.out.println(number1 / number2); // 9
+
+        // remainder or modulus or modulo
+        system.out.println(number1 % number2); // 0
+    }
+}
