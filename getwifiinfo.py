9e2fbf842cc7d772a4f3c861e2fea7be8c00710b
kirti <shuklakirti2004@gmail.com>
2023-10-20 15:04:07 +0530

    GET WIFI INFO

--- getwifiinfo.py
+++ getwifiinfo.py
@@ -4,16 +4,19 @@ print("{:<30} | {:<}".format("Wi-Fi Name", "Password"))
 print("__________________________________________________")
 
 
-wifi_profiles = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace")
-profiles = [line.split(":")[1].strip() for line in wifi_profiles.split('\n') if "All User Profile" in line]
+wifi_profiles = subprocess.check_output(
+    ['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace")
+profiles = [line.split(":")[1].strip() for line in wifi_profiles.split(
+    '\n') if "All User Profile" in line]
 
 for profile in profiles:
     try:
         # Get the password for the Wi-Fi profile
-        result = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8', errors="backslashreplace")
-        password_lines = [line.split(":")[1].strip() for line in result.split('\n') if "Key Content" in line]
+        result = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode(
+            'utf-8', errors="backslashreplace")
+        password_lines = [line.split(":")[1].strip(
+        ) for line in result.split('\n') if "Key Content" in line]
 
-        
         if password_lines:
             print("{:<30} | {:<}".format(profile, password_lines[0]))
         else:
