ced04f21930e93a7f8c95e9d2f28aa94495e1ce7
sundarrr <sundarrathinavel1@gmail.com>
2024-10-05 02:36:27 -0400

    Update Remove_Nth_Element_From_The_LinkedList_In_Reverse.java
    Added Comments and hardcoded one test case for illustration

--- Remove_Nth_Element_From_The_LinkedList_In_Reverse.java
+++ Remove_Nth_Element_From_The_LinkedList_In_Reverse.java
@@ -1,5 +1,6 @@
+package practice;
 
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
     ListNode() {}
@@ -7,24 +8,28 @@ public class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode slow=head;
+public class Solution {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        // Initializing two pointers slow and fast
+    	//FIrst fast pointed moves n number of times and slow pointer will start when fast pointer reaches n
+    	ListNode slow=head;
         ListNode fast=head;
         for(int i=0;i<n;i++)
         {
             fast=fast.next;
         }
+        // If fast pointer reaches null then it means that the n is the size of the linked list hence returning from the second node.
         if(fast==null)
         {
             return head.next;
         }
+        //When fast pointer is not null we move it till last element
         while(fast.next!=null)
         {
             slow=slow.next;
-            System.out.println(slow.val);
             fast=fast.next;
         }
+        // Now pointing the slow pointer to the next of target node
         ListNode temp = slow.next;
         if(temp!=null)
         {
@@ -34,6 +39,32 @@ class Solution {
         {
             slow.next=null;
         }
+        //Finally returning the head of the linked list
         return head;
+    }
+    public static void main(String args[])
+    {
+    	ListNode start = new ListNode(3);
+    	ListNode check = start;
+    	ListNode remove =start;
+    	start.next = new ListNode(21);
+    	start.next.next =  new ListNode (323);
+    	start.next.next.next = new ListNode (456);
+    	System.out.println("Before removing 2nd last node");
+    	while(start != null)
+    	{
+    		System.out.print(start.val+" ");
+    		start=start.next;
+    	}
+    	System.out.println();
+    	System.out.println("After removing 2nd last node");
+    	removeNthFromEnd(remove, 2);
+
+    	while(check != null)
+    	{
+    		System.out.print(check.val+" ");
+    		check=check.next;
+    	}
+
     }
 }
